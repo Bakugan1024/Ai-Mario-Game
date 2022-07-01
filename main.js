b2cd8b9237b572218cf60f8a98f588a8ff60af81
Bakugan1024 <85323092+Bakugan1024@users.noreply.github.com>
2022-07-01 18:39:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+noseX = "";
+noseY = "";
+GameStatus = "";
+
+function startGame()
+{
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loading";
+}
+
+
+function game(){
+	instializeInDraw();
+	moveEnvironment(mario);
+	drawSprites();
+}
+
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
@@ -10,12 +28,33 @@ function setup() {
 
 
 	instializeInSetup(mario);
+
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+
+
+function gotPoses(){
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
 }
 
 function draw() {
 	game()
 }
-
 
 
 
